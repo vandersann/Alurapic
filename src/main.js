655fc3c19ed7a10c110d33784f7a41a6b2e791f3
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2022-01-10 18:16:20 -0300

    Configurando Rotas

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
+// alurapic/src/main.js
+
+import Vue from 'vue'
+import App from './App.vue'
 import VueResource from 'vue-resource';
+// importando o router!
+import VueRouter from 'vue-router';
 
 
 Vue.use(VueResource);
-
-
-
-
+Vue.use(VueRouter);
+// registrando o router
 
 new Vue({
   el: '#app',
