7a0db7faef01e7cce1f88959ff6bdea601108518
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2022-01-11 18:56:05 -0300

    Acrescentando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
+<!-- alurapic/src/App.vue -->
 
 <template>
   <div class="corpo">
 
-    <nav>
-      <ul>
-          <li v-for="route in routes">
-            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
-          </li>
-      </ul>
-    </nav>
+    <meu-menu :rotas="routes"/>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
 
   </div>
 </template>
 <script>
 
 import { routes }  from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
 
   data() {
 
@@ -31,14 +33,18 @@ export default {
 
 }
 </script>
-
-<style>
 <style>
 
-.corpo {
+  .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
   }
 
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
 </style>
