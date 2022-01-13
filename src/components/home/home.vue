734118ba99b2227770e8d278493945231b59f458
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2022-01-13 17:29:29 -0300

    Conclusão do botão

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,18 +1,19 @@
-<!-- alurapic/src/components/home/Home.vue -->
-
 <template>
     <div>
-
         <h1 class="centralizado">Alurapic</h1>
-
         <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
         <ul class="lista-fotos">
-        <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-            <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-            </meu-painel>
-        </li>
+          <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+              <meu-painel :titulo="foto.titulo">
+                <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+                <meu-botao
+  rotulo="remover"
+  tipo="button"
+  :confirmacao="true"
+  @botaoAtivado="remove(foto)"
+  estilo="perigo"/>
+              </meu-painel>
+          </li>
         </ul>
     </div>
 </template>
@@ -21,20 +22,29 @@
 
 import Painel from '../shared/painel/Painel.vue';
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
 
 export default {
 
   components: {
 
     'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
   },
 
-  data () {
+  methods: {
+
+    remove(foto) {
+      // exibindo o título da foto selecionado
+      alert(foto.titulo);
+    }
+  },
+
+  data() {
     return {
-
+      titulo: 'Alurapic',
       fotos: [],
-
       filtro: ''
     }
   },
